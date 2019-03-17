25137310a3822b0fef3f21926a28ad52592aa12f
omoskalenko <moskalenkogm@gmail.com>
2019-03-18 00:14:37 +0300

    Добавлена проверка существования даты

--- src/components/Create/CreateForm.vue
+++ src/components/Create/CreateForm.vue
@@ -1,85 +1,83 @@
 <template>
+  <div class="create_certificate">
+    <h2 v-if="create">Сертификат №{{setNumber}} создан</h2>
+    <div class="certificate_card" v-show="!create">
+      <h2>Новый сертификат</h2>
+      <form @submit="save" class="ui form">
+        <div class="field">
+          <table>
+            <tr>
+              <td align="left">
+                <label for="number">Номер</label>
+                <p v-show="error_message">{{error_message}}</p>
+              </td>
+              <td>
+                <input type="text" name="number" v-model="number" disabled>
+              </td>
+            </tr>
+            <tr>
+              <td align="left">
+                <label for="name">Имя</label>
+              </td>
+              <td>
+                <input type="text" name="name" v-model="name">
+              </td>
+            </tr>
+            <span style="color: tomato">{{ fieldErrors.name }}</span>
+            <tr>
+              <td align="left">
+                <label for="soname">Фамилия</label>
+              </td>
+              <td>
+                <input type="text" name="soname" v-model="soname">
+              </td>
+            </tr>
+            <span style="color: tomato">{{ fieldErrors.soname }}</span>
+            <tr>
+              <td align="left">
+                <label for="phname">Отчество</label>
+              </td>
+              <td>
+                <input type="text" name="phname" v-model="phname">
+              </td>
+            </tr>
+            <span style="color: tomato">{{ fieldErrors.phname }}</span>
+            <tr>
+              <td align="left">
+                <label for="birthday">Дата рождения</label>
+              </td>
+              <td>
+                <input type="text" name="birthday" v-model="birthday" placeholder="ДД.ММ.ГГГ">
+              </td>
+            </tr>
+            <span style="color: tomato">{{ fieldErrors.birthday }}</span>
+            <tr>
+              <td align="left">
+                <label for="email">E-mail</label>
+              </td>
+              <td>
+                <input type="text" name="email" v-model="email" placeholder="example@example.com">
+              </td>
+            </tr>
+            <span style="color: tomato">{{ fieldErrors.email }}</span>
+          </table>
+        </div>
+        <button class="btn btn-outline-dark">Сохранить</button>
+      </form>
+    </div>
 
-<div class="create_certificate">
-  <h2 v-if="create">Сертификат №{{setNumber}} создан</h2>
-  <div class="certificate_card" v-show="!create">
-    <h2>Новый сертификат</h2>
-    <form @submit="save" class="ui form">
-      <div class="field">
-        <table>
-          <tr>
-            <td align="left">
-              <label for="number">Номер</label>
-              <p v-show="error_message">{{error_message}}</p>
-            </td>
-            <td>
-              <input type="text" name="number" v-model="number" disabled>
-            </td>
-          </tr>
-          <tr>
-            <td align="left">
-              <label for="name">Имя</label>
-            </td>
-            <td>
-              <input type="text" name="name" v-model="name">
-            </td>
-          </tr>
-          <span style="color: tomato">{{ fieldErrors.name }}</span>
-          <tr>
-            <td align="left">
-              <label for="soname">Фамилия</label>
-            </td>
-            <td>
-              <input type="text" name="soname" v-model="soname">   
-            </td>            
-          </tr>
-          <span style="color: tomato">{{ fieldErrors.soname }}</span>
-          <tr>
-            <td align="left">
-              <label for="phname">Отчество</label>
-            </td>
-            <td>
-              <input type="text" name="phname" v-model="phname">
-            </td>            
-          </tr>
-          <span style="color: tomato">{{ fieldErrors.phname }}</span>
-          <tr>
-            <td align="left">
-              <label for="birthday">Дата рождения</label>
-            </td>
-            <td>
-              <input type="text" name="birthday" v-model="birthday" placeholder="ДД.ММ.ГГГ">              
-            </td>           
-          </tr>
-           <span style="color: tomato">{{ fieldErrors.birthday }}</span>
-          <tr>
-            <td align="left">
-              <label for="email">E-mail</label>
-            </td>
-            <td>
-              <input type="text" name="email" v-model="email" placeholder="example@example.com">              
-            </td>           
-          </tr>
-           <span style="color: tomato">{{ fieldErrors.email }}</span>
-        </table>
-      </div>
-      <button class="btn btn-outline-dark">Сохранить</button>
-    </form>
-  </div>
-
-  <div class="history">
-    <h2>История</h2>
-    <hr>
-    <ul>
-      <li v-for="num in history" v-bind:key="num">{{num}}</li>
-    </ul>
-  </div>
-
+    <div class="history">
+      <h2>История</h2>
+      <hr>
+      <ul>
+        <li v-for="num in history" v-bind:key="num">{{num}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import CertificateCard from '../CertificateCard/CertificateCard.vue'
+import CertificateCard from "../CertificateCard/CertificateCard.vue";
 
 export default {
   name: "CreateForm",
@@ -89,39 +87,38 @@ export default {
 
   data() {
     return {
-      name: '',
-      soname: '',
-      phname: '',
-      birthday: '',
-      email: '',
+      name: "",
+      soname: "",
+      phname: "",
+      birthday: "",
+      email: "",
       create: false,
-      error_message: '',
-      setNumber: '',
+      error_message: "",
+      setNumber: "",
       fieldErrors: {
         name: undefined,
         soname: undefined,
         phname: undefined,
         birthday: undefined,
-        email: undefined,
+        email: undefined
       }
-    }
+    };
   },
 
   computed: {
-     number() {
-         return `9905${Math.floor(Math.random() * (999999 - 100000)) + 100000}`;
-     },
+    number() {
+      return `9905${Math.floor(Math.random() * (999999 - 100000)) + 100000}`;
+    },
 
-     history() {
-       return this.$store.getters.history;
-     }
-
+    history() {
+      return this.$store.getters.history;
+    }
   },
 
   methods: {
     save(evt) {
       evt.preventDefault();
-      
+
       let data = {
         number: evt.target.number.value,
         name: evt.target.name.value,
@@ -136,30 +133,30 @@ export default {
         console.log(this.fieldErrors);
         console.log(Object.keys(this.fieldErrors));
         return;
-      };
-      
+      }
+
       this.$store
         .dispatch("createCertificate", data)
         .then(res => {
           this.toogleState();
-          this.setNumber = res.data.number
+          this.setNumber = res.data.number;
           setTimeout(() => {
             this.resetForm();
-          }, 2000)
-        }).catch(error => {
+          }, 2000);
+        })
+        .catch(error => {
           this.error_message = error;
         });
     },
 
-
     resetForm() {
       this.toogleState();
-      this.name = '',
-      this.soname = '',
-      this.phname = '',
-      this.birthday = '',
-      this.email = '',
-      this.error_message = '';
+      (this.name = ""),
+        (this.soname = ""),
+        (this.phname = ""),
+        (this.birthday = ""),
+        (this.email = ""),
+        (this.error_message = "");
     },
 
     toogleState() {
@@ -180,20 +177,29 @@ export default {
       console.log(fields);
       const errors = {};
 
-      if (!this.isValidName(fields.name)) errors.name = "Имя должно состоятьиз букв русского алфавита и не превышать 60 символов";
-      if (!this.isValidSoname(fields.soname)) errors.soname = "Фамилия должна состоятьиз букв русского алфавита и не превышать 60 символов";
-      if (!this.isValidPhname(fields.phname)) errors.phname = "Отчество должно состоятьиз букв русского алфавита и не превышать 60 символов";
-      if (!this.isValidBirthday(fields.birthday)) errors.birthday = "Дата рождения должна быть в формате ДД.ММ.ГГГГ, год рождения от 1998";
-      if (!this.isValidEmail(fields.messages_email)) errors.email = "Адрес электронной почты: ограничение в 255 символов";
-    
+      if (!this.isValidName(fields.name))
+        errors.name =
+          "Имя должно состоятьиз букв русского алфавита и не превышать 60 символов";
+      if (!this.isValidSoname(fields.soname))
+        errors.soname =
+          "Фамилия должна состоятьиз букв русского алфавита и не превышать 60 символов";
+      if (!this.isValidPhname(fields.phname))
+        errors.phname =
+          "Отчество должно состоятьиз букв русского алфавита и не превышать 60 символов";
+      if (!this.isValidBirthday(fields.birthday))
+        errors.birthday =
+          "Проверьте корректность введенной даты. Дата рождения должна быть в формате ДД.ММ.ГГГГ, год рождения от 1998";
+      if (!this.isValidEmail(fields.messages_email))
+        errors.email = "Адрес электронной почты: ограничение в 255 символов";
+
       return errors;
     },
 
     isValidName(name) {
-      const regExpName = /^[а-яА-ЯёЁ]+([-]{1}[а-яА-ЯёЁ]+){0,3}$/;      
+      const regExpName = /^[а-яА-ЯёЁ]+([-]{1}[а-яА-ЯёЁ]+){0,3}$/;
       return regExpName.test(name);
     },
-    
+
     isValidSoname(soname) {
       const regExpSoname = /^[а-яА-ЯёЁ]+([ -]{1}[а-яА-ЯёЁ]+){0,3}$/;
       return regExpSoname.test(soname);
@@ -211,17 +217,30 @@ export default {
 
     isValidBirthday(date) {
       const regExpBirthday = /^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/;
-      
-      return regExpBirthday.test(date) 
-      && date.match(regExpBirthday)[3] >= 1998
-      && date.match(regExpBirthday)[1] >= 1
-      && date.match(regExpBirthday)[1] <= 31
-      && date.match(regExpBirthday)[2] >= 1
-      && date.match(regExpBirthday)[2] <= 12;
+      //проверка существования даты 
+      function validDate(date) {
+        var d_arr = date.split(".");
+        var d = new Date(d_arr[2] + "/" + d_arr[1] + "/" + d_arr[0] + ""); // дата в 
+        if (
+          d_arr[2] != d.getFullYear() ||
+          d_arr[1] != d.getMonth() + 1 ||
+          d_arr[0] != d.getDate()
+        ) {
+          return false; // неккоректная дата
+        }
+        return true;
+      }
 
-    },
-
-
+      return (
+        regExpBirthday.test(date) &&
+        date.match(regExpBirthday)[3] >= 1998 &&
+        date.match(regExpBirthday)[1] >= 1 &&
+        date.match(regExpBirthday)[1] <= 31 &&
+        date.match(regExpBirthday)[2] >= 1 &&
+        date.match(regExpBirthday)[2] <= 12
+        && validDate(date)
+      );
+    }
   }
 };
 </script>
